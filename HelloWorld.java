272f1b84956c8f84102b219dc02610e902f8f441
saikumarb <saikumar.beeraka@alacriti.com>
2022-04-07 15:34:30 +0530

    sys changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,7 @@
 class HelloWorld{
 public static void main(String args[]){
   System.out.println("hello world....");
-  System.out.println("Hi ANil......");
-System.out.println("Hi ANil......");
-  System.out.println("Hi saiii......");
-    System.out.println("Hi saikumar......");
+ 
 
 }
 }
