1375d53896bb9b9f5508b5a5d206b7fad9d151a0
anilm <anil.mergoju@alacriti.com>
2022-04-07 16:23:23 +0530

    resolve conflicts by mergetool

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,11 @@ public static void main(String args[]){
   System.out.println("Hi ANil......");
     System.out.println("Hi Ramya......");
   System.out.println("Hi Sai......");
-    System.out.println("Hi Sai......");
+ System.out.println("Hi Sai......");
+	int a=10;
+	int b=30;
+	System.out.println(a+b);
+   
 
 }
 }
