481a99cfc8cb83a1255c822524b6d74b11842bfc
anilm <anilmergoju@gmail.com>
2022-04-07 15:37:19 +0530

    Changes are done

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 class HelloWorld{
 public static void main(String args[]){
   System.out.println("hello world....");
- 
+System.out.println("Hi kalyani......");
+System.out.println("Hi Ramya......");
 
 }
 }
